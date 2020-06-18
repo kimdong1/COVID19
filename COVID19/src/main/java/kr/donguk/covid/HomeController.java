4e767fc2bf5ac898ed9bf354ccf4156f7b2a6759
김동욱 <hi_manse@naver.com>
2020-06-18 20:57:06 +0900

    sync

--- COVID19/src/main/java/kr/donguk/covid/HomeController.java
+++ COVID19/src/main/java/kr/donguk/covid/HomeController.java
@@ -1,11 +1,13 @@
 package kr.donguk.covid;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,22 +19,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		StringBuffer sb = new StringBuffer();
+		try {
+			String url = ("http://ncov.mohw.go.kr/");
+			
+			Document doc = Jsoup.connect(url).get(); 
+			Elements el = doc.select("div.rpsam_graph");
+			
+			
+			for(Element e : el.select("button")) {
+				System.out.println(e.html());
+				sb.append(e.text()+"<br />");
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		model.addAttribute("text",sb.toString());
 		return "home";
 	}
 	
